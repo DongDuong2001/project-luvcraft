9cc2eed394460ad1bd929c808ed33dbb697653e7
Duong Phu Dong <dongduong840@gmail.com>
2026-04-30 11:08:44 +0700

    feat(frontend): wrap main component with IBM Plex Sans font styling

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { IBM_Plex_Sans } from 'next/font/google'
 import { DashboardProvider } from '../state/dashboardContext'
+
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  variable: '--font-body',
+  weight: ['400', '500', '600', '700'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DashboardProvider>
-      <Component {...pageProps} />
-    </DashboardProvider>
+    <main className={ibmPlexSans.variable}>
+      <DashboardProvider>
+        <Component {...pageProps} />
+      </DashboardProvider>
+    </main>
   )
 }
