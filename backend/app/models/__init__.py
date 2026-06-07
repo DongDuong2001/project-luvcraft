185d98a9edc45cee0f8db9a7799aedfa48751579
QDong26 <s3938006@rmit.edu.vn>
2026-06-07 18:53:19 +0700

    feat: add quality control models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -8,5 +8,6 @@ from app.models.synthesis import SynthesisOutput
 from app.models.evaluation import GeneratedReport, ModelVersion, EvaluationRun
 from app.models.brand import BrandProfile, CollaborationCandidate, PreviousCollab
 from app.models.collection import CollectedSignal, SignalMetric
+from app.models.quality import FilterAudit, FilterSummary
 
 # Huy will add his imports here:
