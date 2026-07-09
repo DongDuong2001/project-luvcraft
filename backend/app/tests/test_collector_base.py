386743bd6577660f560026506636f4b56bce9e7d
Duong Phu Dong <dongduong840@gmail.com>
2026-07-09 17:35:05 +0700

    test(collectors): remove CommunityCollector from stub tests parametrization

--- backend/app/tests/test_collector_base.py
+++ backend/app/tests/test_collector_base.py
@@ -161,7 +161,7 @@ def test_get_json_raises_malformed_response_for_non_dict_payload():
         collector._get_json("/search", {})
 
 
-@pytest.mark.parametrize("collector_cls", [CommunityCollector, HypeCollector, SocialCollector])
+@pytest.mark.parametrize("collector_cls", [HypeCollector, SocialCollector])
 def test_stub_collectors_follow_the_shared_interface(collector_cls):
     collector = collector_cls()
 
